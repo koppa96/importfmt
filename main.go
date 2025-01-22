54d9a63a98008e11cc9e98155aa0acc44c007f77
Koppa Péter <koppa.peter@encosoft.hu>
2025-01-22 12:27:53 +0100

    Chain golines invocation

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
@@ -15,7 +17,9 @@ var cmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		write, _ := cmd.Flags().GetBool("write")
 		if write && len(args) == 0 {
-			return fmt.Errorf("the argument 'filename' must be specified when the flag 'write' is set")
+			return fmt.Errorf(
+				"the argument 'filename' must be specified when the flag 'write' is set",
+			)
 		}
 
 		f, err := readFile(args...)
@@ -23,25 +27,65 @@ var cmd = &cobra.Command{
 			return err
 		}
 
-		var writer io.Writer
-		if len(args) > 0 && write {
+		var buf bytes.Buffer
+		err = format(f, &buf)
+		if err != nil {
+			return fmt.Errorf("failed to format file: %w", err)
+		}
+
+		runGolines, _ := cmd.Flags().GetBool("invoke-golines")
+		if runGolines {
+			reader := bytes.NewReader(buf.Bytes())
+
+			var golinesBuf bytes.Buffer
+			err = invokeGolines(reader, &golinesBuf)
+			if err != nil {
+				return err
+			}
+
+			buf = golinesBuf
+		}
+
+		if write {
 			file, err := os.Create(args[0])
 			if err != nil {
 				return fmt.Errorf("failed to create file '%s': %w", args[0], err)
 			}
-			defer file.Close()
 
-			writer = file
+			_, err = buf.WriteTo(file)
+			if err != nil {
+				return fmt.Errorf("failed to write formatted file to '%s': %w", args[0], err)
+			}
+
+			defer file.Close()
 		} else {
-			writer = os.Stdout
+			_, err = buf.WriteTo(os.Stdout)
+			if err != nil {
+				return fmt.Errorf("failed to write formatted file to stdout: %w", err)
+			}
 		}
 
-		return format(f, writer)
+		return nil
 	},
+}
+
+func invokeGolines(reader io.Reader, writer io.Writer) error {
+	cmd := exec.Command("golines")
+	cmd.Stdin = reader
+	cmd.Stdout = writer
+
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to invoke golines on file: %w", err)
+	}
+
+	return nil
 }
 
 func main() {
 	cmd.Flags().BoolP("write", "w", false, "Set to true to write the result to the file")
+	cmd.Flags().
+		Bool("invoke-golines", false, "If set to true, then after formatting, this tool invokes golines to format the rest of the file")
 
 	if err := cmd.Execute(); err != nil {
 		println(err)
